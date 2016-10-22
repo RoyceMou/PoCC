d9368283ec0d0690d5098e5ead4c72a3ca748604
Royce Mou <Royce Mou>
2016-10-22 14:47:43 -0700

    Modified playbook

--- Assignment 2/bootstrap.sh
+++ Assignment 2/bootstrap.sh
@@ -27,6 +27,27 @@
 # echo "Ansible Configured"
 
 # run server playbook
+
+# export OS_AUTH_URL=https://keystone.isis.vanderbilt.edu:5000/v2.0
+
+# # With the addition of Keystone we have standardized on the term **tenant**
+# # as the entity that owns the resources.
+# export OS_TENANT_ID=7b7cacfc1d3c4973a2666a26cf1a40ff
+# export OS_TENANT_NAME="Cloud Class"
+
+# # In addition to the owning entity (tenant), openstack stores the entity
+# # performing the action as the **user**.
+# export OS_USERNAME="mour"
+
+# # With Keystone you pass the keystone password.
+# echo "Please enter your OpenStack Password: "
+# read -sr OS_PASSWORD_INPUT
+# export OS_PASSWORD=$OS_PASSWORD_INPUT
+
+# # If your configuration has multiple regions, we set that information here.
+# # OS_REGION_NAME is optional and only valid in certain environments.
+# export OS_REGION_NAME="regionOne"
+
 ssh-agent bash
 ssh-add $SSH_PEM
 # chmod 700 /vagrant/playbook_server.yml
